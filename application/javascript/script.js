d799b8b0818b085b52d5691e6dcd4ec71dde0dae
Nathan099-dev <tonininathan8@gmail.com>
2024-12-12 14:21:54 -0300

    criando função js

---
+++ application/javascript/script.js
@@ -0,0 +1,9 @@
+function adicionarNovaTarefa(){
+    const taskField = document.getElementById('inputTarefa');
+
+    if (taskField == "") {
+        alert('erro');
+    }
+}
+
+adicionarNovaTarefa()
